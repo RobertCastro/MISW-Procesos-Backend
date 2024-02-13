df5f030d8b031f474e3e33704030058df44c09b9
DaGamez <da.gamez96@uniandes.edu.co>
2024-02-13 18:30:58 -0500

    PA_12: enumeracion roles (rojo)

--- tests/test_obtener_enums.py
+++ tests/test_obtener_enums.py
@@ -27,3 +27,17 @@ def test_tipo_movimientos(client):
     assert len(response_json) == 2
     assert 'INGRESO' in response_json
     assert 'EGRESO' in response_json
+
+def test_roles(client):
+    usuario_1 = Usuario(usuario='usuario_1', contrasena='123456')
+    db.session.add(usuario_1)
+    db.session.commit()
+
+    token_usuario_1 = create_access_token(identity=usuario_1.id)
+    headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {token_usuario_1}'}
+    response = client.get('/roles', headers=headers)
+    response_json = response.json
+    assert isinstance(response_json, list)
+    assert len(response_json) == 2
+    assert 'ADMINISTRADOR' in response_json
+    assert 'PROPIETARIO' in response_json
