5a8ea3109446879009291e5b82ddff8eaaed6ee0
DaGamez <da.gamez96@uniandes.edu.co>
2024-03-09 11:33:31 -0500

    PA_17_des: se quitan prints

--- vistas/vista_editar_mantenimiento.py
+++ vistas/vista_editar_mantenimiento.py
@@ -12,8 +12,6 @@ class VistaEditarMantenimientos(Resource):
     
     @jwt_required()
     def put(self, id_propiedad, id_mantenimiento):
-        print('Editar mantenimiento id_propiedad:', id_propiedad)
-        print('Editar mantenimiento id_mantenimiento:', id_mantenimiento)
 
         try:
             rol = current_user.rol.value
@@ -26,28 +24,11 @@ class VistaEditarMantenimientos(Resource):
             if not propiedad:
                 return {"error": "La propiedad no existe"}, 404            
             
-            print('current user: ', current_user.id)
-            print('id_mantenimiento:', id_mantenimiento)
             resultado_buscar_mantenimiento = buscar_mantenimiento(id_mantenimiento, current_user.id)
             if resultado_buscar_mantenimiento.error:
                 return resultado_buscar_mantenimiento.error
             
             mantenimiento = resultado_buscar_mantenimiento.mantenimiento
-
-            print('mantenimiento:', mantenimiento)
-            print('request.json:', request.json)
-            print('ACTUAL mantenimiento.id:', mantenimiento.id)
-            print('NUEVO mantenimiento.id:', request.json.get('id'))
-            print('ACTUAL mantenimiento.id_propiedad:', mantenimiento.id_propiedad)
-            print('NUEVO mantenimiento.id_propiedad:', request.json.get('id_propiedad'))
-            print('ACTUAL mantenimiento.id_usuario:', mantenimiento.id_usuario)
-            print('NUEVO mantenimiento.id_usuario:', request.json.get('id_usuario'))
-            print('ACTUAL mantenimiento.tipo_mantenimiento:', mantenimiento.tipo_mantenimiento)
-            print('NUEVO mantenimiento.tipo_mantenimiento:', request.json.get('tipo_mantenimiento'))
-    
-
-
-           
             mantenimiento_en_edicion = json.loads(json.dumps(request.json))
             
             mantenimiento_schema.load(mantenimiento_en_edicion, 
